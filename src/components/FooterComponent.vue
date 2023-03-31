a3cfa99fb9bfb4bd4746f6a4dd5a489c9a95c438
Sinclairr08 <ljhjoon00@gmail.com>
2023-03-31 23:10:49 +0900

    chore: Change license phrase

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -2,9 +2,10 @@
   <div
     class="w-full absolute bottom-0 p-3 flex flex-col text-center text-xs bg-cyan-400 text-white"
   >
-    <span
-      >본 페이지의 모든 정보 및 리소스에 대한 권리는 넥슨코리아에 있습니다</span
+    <span>© 2023. e-kivotos </span>
+    <span>
+      e-kivotos는 넥슨 게임즈의 공식 서비스가 아니며, 모든 리소스의 저작권은
+      저작권자에게 있습니다</span
     >
-    <span> 이 사이트는 블루 아카이브의 비공식 팬 페이지입니다</span>
   </div>
 </template>
