96ab8fe9cbad63ce9516661146201c173e505ed6
Sinclairr08 <ljhjoon00@gmail.com>
2023-04-04 19:44:25 +0900

    refactor: Add backend code to this project

---
+++ backend/src/main/java/sinclairr08/ekivotosserver/EKivotosServerApplication.java
@@ -0,0 +1,13 @@
+package sinclairr08.ekivotosserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EKivotosServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EKivotosServerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/sinclairr08/ekivotosserver/EKivotosServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sinclairr08.ekivotosserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EKivotosServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
