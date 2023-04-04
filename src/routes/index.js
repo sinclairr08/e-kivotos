2edacbdf3342b99bf23cf7d7b12358f91c9a53de
Sinclairr08 <ljhjoon00@gmail.com>
2023-04-04 12:50:35 +0900

    fix: Fix 404 error

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import CostPage from "../components/CostPage";
 import MainPage from "../components/MainPage";
 import VersionPage from "../components/VersionPage";
 
 export default new createRouter({
-  history: createWebHistory("/e-kivotos"),
+  history: createWebHashHistory("/e-kivotos"),
   routes: [
     { path: "/", component: MainPage },
     { path: "/cost", component: CostPage },
