3d73c4c7d2f595c24f40598240fdcfb1369a21c2
Sinclairr08 <ljhjoon00@gmail.com>
2023-04-18 20:08:07 +0900

    fix: Solve cors issue by allowing all origin

--- backend/src/main/java/sinclairr08/ekivotosserver/controller/VersionController.java
+++ backend/src/main/java/sinclairr08/ekivotosserver/controller/VersionController.java
@@ -1,17 +1,16 @@
 package sinclairr08.ekivotosserver.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import sinclairr08.ekivotosserver.version.Version;
 import sinclairr08.ekivotosserver.version.VersionRepository;
 
 import java.util.List;
 
-@CrossOrigin(origins = {"https://sinclairr08.github.io", "http://localhost:23908"})
-@Controller
+@CrossOrigin
+@RestController
 public class VersionController {
     private final VersionRepository versionRepository;
 
@@ -21,7 +20,6 @@ public class VersionController {
     }
 
     @GetMapping(value = "/versions")
-    @ResponseBody
     public List<Version> versionJson() {
         return versionRepository.findAll();
     }
