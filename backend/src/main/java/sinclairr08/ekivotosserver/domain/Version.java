e091bad8ade51cb59e72f1ba17bedd46dc67e429
Sinclairr08 <ljhjoon00@gmail.com>
2023-06-16 21:11:42 +0900

    fix: Change data type
    Version release date: String -> LocalDate

--- backend/src/main/java/sinclairr08/ekivotosserver/domain/Version.java
+++ backend/src/main/java/sinclairr08/ekivotosserver/domain/Version.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDate;
+
 @Getter
 @Entity
 public class Version {
@@ -12,10 +14,10 @@ public class Version {
     private Long id;
 
     private String versionString;
-    private String releaseDate;
+    private LocalDate releaseDate;
     private String updates;
 
-    public void setVersion(String versionString, String releaseDate, String updates) {
+    public void setVersion(String versionString, LocalDate releaseDate, String updates) {
         this.versionString = versionString;
         this.releaseDate = releaseDate;
         this.updates = updates;
--- backend/src/main/java/sinclairr08/ekivotosserver/dto/VersionDto.java
+++ backend/src/main/java/sinclairr08/ekivotosserver/dto/VersionDto.java
@@ -2,18 +2,16 @@ package sinclairr08.ekivotosserver.dto;
 
 import lombok.Getter;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Getter
 public class VersionDto {
     private String versionString;
-    private String releaseDate;
+    private LocalDate releaseDate;
     private List<String> updates;
 
-    public VersionDto() {
-    }
-
-    public VersionDto(String versionString, String releaseDate, List<String> updates) {
+    public VersionDto(String versionString, LocalDate releaseDate, List<String> updates) {
         this.versionString = versionString;
         this.releaseDate = releaseDate;
         this.updates = updates;
